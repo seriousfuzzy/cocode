a8dde65a6d66682821d4592468322dede0493f43
seriousfuzzy <seriousfuzzy1211@gmail.com>
2024-08-15 15:55:50 +0900

    import fix, and gitignore

--- modules/resume.py
+++ modules/resume.py
@@ -3,6 +3,7 @@
 from langchain_community.document_loaders import PyPDFLoader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain_community.vectorstores import Chroma
+from langchain_community.embeddings.ollama import OllamaEmbeddings
 import tempfile
 import os
 
